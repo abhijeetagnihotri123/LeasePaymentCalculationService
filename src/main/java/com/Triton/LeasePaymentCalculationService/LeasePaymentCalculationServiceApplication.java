1d9e144b4eabd7848cce29709c99690c177ff0a7
abhijeetagnihotri123 <abhijeetagnihotri5@gmail.com>
2024-08-12 20:52:36 +0530

    More code added

---
+++ src/main/java/com/Triton/LeasePaymentCalculationService/LeasePaymentCalculationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Triton.LeasePaymentCalculationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeasePaymentCalculationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeasePaymentCalculationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Triton/LeasePaymentCalculationService/LeasePaymentCalculationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Triton.LeasePaymentCalculationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeasePaymentCalculationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
